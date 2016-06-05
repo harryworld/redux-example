79a27aceb3803fdcfae4e18d472eb1792e4e16f6
Harry Ng <harryworld@gmail.com>
2016-06-05 22:53:29 +0800

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+render(
+  document.getElementById('root')
+)
